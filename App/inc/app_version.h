7e30c3216a5c6c50960db944d8d150193fb6e5df
zhouzhanhao <zhouzhanhao@openloong.net>
2026-08-26 16:03:25 +0800

    更新固件版本号，同步更新烧录软件

--- App/inc/app_version.h
+++ App/inc/app_version.h
@@ -10,8 +10,20 @@ extern "C" {
  * 发布新固件时只需在此处修改，串口日志和通信接口会同步更新。
  */
 #define GLOVE_FW_VERSION_MAJOR             2
-#define GLOVE_FW_VERSION_MINOR             1
+#define GLOVE_FW_VERSION_MINOR             2
 #define GLOVE_FW_VERSION_PATCH             0
+
+/*
+    版本说明
+    V2.2.0： 
+        1. 485通讯频率提升至 200Hz（固件支持，且上位机可以实现）
+        2. IMU CAN通讯 默认通讯速度为1Mbps
+        3. 传感器采样频率提高到 200Hz
+        4. 解决一些已知问题
+
+    V1.0.0:
+         1. 初始版本 
+*/
 
 #define GLOVE_FW_VERSION_STRINGIFY_INNER(value)  #value
 #define GLOVE_FW_VERSION_STRINGIFY(value)        GLOVE_FW_VERSION_STRINGIFY_INNER(value)
